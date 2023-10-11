08406977b41acf5bee67b922bcdc61b972690a6a
Uamirfarooq <umair.codedeveloper@gmail.com>
2023-10-10 19:11:06 -0700

    working

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className=" text-red-600">Hellow word</h1>
+      <Button>this is button</Button>
+    </>
+  );
+}
